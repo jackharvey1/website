5692192fbd79e83f1ee0ef67fcd20ceeb7a4ebeb
Jack Harvey <jack.harvey@news.co.uk>
2017-03-04 18:19:38 +0000

    favicon, external links, general rearranging

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,10 +1,19 @@
+/* eslint no-extend-native: off */
 Object.prototype.document = window.document;
 Object.prototype.location = window.location;
 const $ = require('zepto').$;
-delete(Object.prototype.document);
-delete(Object.prototype.location);
+delete Object.prototype.document;
+delete Object.prototype.location;
 
 window.onload = function () {
-    const colourNumber = Math.round(Math.random() * 2) + 1;
-    $('#banner').addClass(`bannerColour${colourNumber}`);
+    setInterval(cycleBannerColour(), 10 * 1000);
 };
+
+function cycleBannerColour () {
+    let i = 0;
+    return function () {
+        $(`#background-${i}`).removeClass(`Background--off`);
+        i = i === 2 ? 0 : i + 1;
+        $(`#background-${i}`).addClass(`Background--off`);
+    };
+}
